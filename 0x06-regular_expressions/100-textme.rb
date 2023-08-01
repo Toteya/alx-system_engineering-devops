90ab71e7bd735ef63cfc767e05a4a9640d58ba66
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-01 22:36:47 +0200

    Regex: Extract info from a file

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+sender = ARGV[0].scan(/\[from:\+?\w*\]/).join
+receiver = ARGV[0].scan(/\[to:\+?\w*\]/).join
+flags = ARGV[0].scan(/\[flags:-?\d:-?\d:-?\d:-?\d:-?\d\]/).join
+
+sender = sender.scan(/(?<=.{6})\w*[^\+\[\]]/).join
+receiver = receiver.scan(/(?<=.{4})\w*[^\+\[\]]/).join
+flags = flags.scan(/(?<=.{7}).*[^\[\]]/).join
+
+puts "#{sender},#{receiver},#{flags}"
