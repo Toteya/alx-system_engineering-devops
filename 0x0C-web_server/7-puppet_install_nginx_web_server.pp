2274bf5602c20802e6e599ce6300df30b815b88d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-03 13:51:49 +0200

    configure new webserver with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+# This manifest configure a new server with Nginx webser
+
+package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt-get',
+}
+
+$str = "server {
+  \tlisten 80 default_server;
+  \tlisten [::]:80 default_server;\n
+  \troot /var/www/html;\n
+  \tindex index.html index.htm index.nginx-debian.html;\n
+  \tserver_name nyandi.tech;\n
+  \trewrite ^/redirect_me/$ http://nyandi.tech permanent;\n
+  \tlocation / {
+  \t\ttry_files \$uri \$uri/ =404;
+  \t}
+}"
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  backup  => '.bak',
+  content => $str,
+}
+
+service {'nginx':
+  ensure => 'running',
+  enable => 'true',
+  restart
+}
